21df7c3b97a2f55f42829a7ad2bf7e5064579780
jgoceljak <john.goceljak@gmail.com>
2023-07-10 22:53:41 -0400

    05 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(low, high) {
+if (!Number.isInteger(low) || !Number.isInteger(high)){
+    return "ERROR"
+}
+if (low < 0 || high < 0) {
+    return "ERROR"
+}
+if (low > high){
+    let temp = high;
+    high = low;
+    low = temp;
+}
+    let sum = 0;
+for (i = low; i < high+1; i++) {
+    sum += i;
+}
 
+return sum
 };
 
 // Do not edit below this line

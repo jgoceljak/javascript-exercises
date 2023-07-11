27ed81fbf01910189177e030a1e3c204f60707f7
jgoceljak <john.goceljak@gmail.com>
2023-07-10 21:59:49 -0400

    03 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
-};
+const reverseString = function(string) {
+    if (string === "") {
+      return "ERROR";
+    }
+  
+    const result = string.split("");
+    const reversed = result.reverse();
+    const final = reversed.join("");
+  
+    return final;
+  };
 
 // Do not edit below this line
 module.exports = reverseString;

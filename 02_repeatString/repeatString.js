4b2d5107fc499a26a93dfbd1f7d4781286853bfa
jgoceljak <john.goceljak@gmail.com>
2023-07-10 20:17:05 -0400

    02 complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, x) {
+    if (x < 0){
+        return "ERROR"
+    }
 
+    let result = '';
+    for (let i = 0; i < x; i++) {
+        result += string;
+      }
+
+      return result;
 };
 
 // Do not edit below this line

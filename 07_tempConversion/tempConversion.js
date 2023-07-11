c48e1ff0fe3f749c3bf1ede5ef93b7f5fa861d0c
jgoceljak <john.goceljak@gmail.com>
2023-07-10 23:04:20 -0400

    07 done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,22 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+
+  if (!isNaN(temperature)) {
+       let celcius = (temperature - 32) * (5/9);
+       celcius = Math.round(celcius * 10) / 10;
+       return celcius
+  } else {
+    return "ERROR"
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  if (!isNaN(temperature)) {
+    let faren = (temperature * (9/5)) + 32;
+    faren = Math.round(faren * 10) / 10;
+    return faren
+    } else {
+      return "ERROR"
+    }
 };
 
 // Do not edit below this line

db0aced8b4d8f4de98547dc02dba855480d0fe78
jgoceljak <john.goceljak@gmail.com>
2023-07-10 22:43:12 -0400

    04 completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...remove) {
+if (array.length == 0) {
+    return "ERROR"
+}
 
+for (const item of remove){
+    let index = array.indexOf(item)
+    if (index != -1){
+        array.splice(index, 1)
+    }
+}
+return array
 };
 
 // Do not edit below this line

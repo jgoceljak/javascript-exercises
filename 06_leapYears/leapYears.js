3b92323c06ee74ba9802846a086135b5678addde
jgoceljak <john.goceljak@gmail.com>
2023-07-10 22:58:35 -0400

    06 done

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+ if (!Number.isInteger(year)){
+    return "ERROR"
+ }
+if (year%4 == 0){
+    if (year%100 == 0 && !(year%400 == 0)) {
+        return false
+    }else {
+        return true
+    }    
+} return false
 
 };
 
